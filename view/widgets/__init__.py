b8613d2ed0c531b87973bccf5a9a05c90097b866
Matthew Schmidt <historicshark98@gmail.com>
2025-10-19 14:52:15 -0500

    budget progress bar

--- view/widgets/__init__.py
+++ view/widgets/__init__.py
@@ -3,4 +3,4 @@ from .date_edit_fix import DateEditFix
 from .combo_box_fix import ComboBoxFix
 from .date_filter import DateFilter
 from .plot_category import PlotCategory
-
+from .budget_progress_bar import BudgetProgressBar
---
+++ view/widgets/budget_progress_bar.py
@@ -0,0 +1,42 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+from view import colors
+
+class BudgetProgressBar(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.loaded = False
+        self.plot_later = None
+
+    def load(self):
+        if not self.loaded:
+            import matplotlib
+            matplotlib.use('Qt5Agg', force=True)
+            self.figure = Figure(figsize=(8,0.4), facecolor='none')
+            self.ax = self.figure.subplots(1, 1)
+            self.canvas = FigureCanvas(self.figure)
+            self.canvas.setStyleSheet('background-color: transparent;')
+            self.layout = QVBoxLayout()
+            self.layout.setContentsMargins(0, 0, 0, 0)
+            self.layout.addWidget(self.canvas)
+            self.setLayout(self.layout)
+            self.loaded = True
+
+            if self.plot_later:
+                self.plot_bar(**self.plot_later)
+
+    def plot_bar(self, maximum: float, value: float, color=None):
+        """ Specify color, otherwise it is green if value < maximum and red if value > maximum """
+        if self.loaded:
+            self.ax.clear()
+            if color is None:
+                color = colors['green'] if value < maximum else colors['red']
+            self.ax.barh('1', maximum, height=1.0, color='none', edgecolor=colors['fg'])
+            self.ax.barh('1', value-0.2, left=0.2, height=0.6, color=color, edgecolor='none')
+            self.ax.axis('off')
+            self.ax.axis('tight')
+            self.canvas.draw()
+            self.plot_later = None
+        else:
+            self.plot_later = {'maximum': maximum, 'value': value, 'color': color}
